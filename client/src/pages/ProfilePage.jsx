4052625696881635f165bd4af32764f14b0ade6f
developervicky <developer.vicky6@gmail.com>
2024-01-01 21:41:05 +0530

    fix: profile redirect issue

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,8 +1,12 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { UserContext } from "../components/UserContext";
+import { Navigate } from "react-router-dom";
 
 export default function ProfilePage() {
   const { user } = useContext(UserContext);
+  if(!user) {
+    return <Navigate to={"/"} />
+  }
 
   return <div>{user?.fname}</div>;
 }
