6b2c6a9d58f261ac2066c446b9ec29ab4d02e50d
developervicky <developer.vicky6@gmail.com>
2024-01-06 11:17:22 +0530

    feat: loading page

--- client/src/pages/LoadingPage.jsx
+++ client/src/pages/LoadingPage.jsx
@@ -1,5 +1,9 @@
-
+import PacmanLoader from "react-spinners/PacmanLoader";
 export default function LoadingPage() {
-
-  return <div>LoadingPage</div>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-4 text-xl font-semibold text-white ">
+      <PacmanLoader color="#36d7b7" />
+      <p>stop starin me, while i'm loading</p>
+    </div>
+  );
 }
