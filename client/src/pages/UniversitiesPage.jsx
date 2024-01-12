90d8ea3b665386be1fc61266adac31157cad6a06
developervicky <developer.vicky6@gmail.com>
2024-01-12 16:52:20 +0530

    fix: universites style

--- client/src/pages/UniversitiesPage.jsx
+++ client/src/pages/UniversitiesPage.jsx
@@ -20,32 +20,30 @@ export default function UniversityPage() {
       {uniData ? (
         <div className="m-10 flex grow flex-col gap-7 rounded-lg border-2 border-primary2 p-4 text-white">
           <h1 className=" text-xl tracking-wide">Universities</h1>
-          <div className="grid grid-cols-5 gap-3 ">
-            {uniData.map((uni, index) => (
-              <div key={index} className="rounded-lg border-2 border-primary3 ">
-                {uni.map((each, index) => (
-                  <Link
-                    to={
-                      user._id == each._id
-                        ? `/user/profile/${each?._id}/`
-                        : `/user/univ/${each?._id}`
-                    }
-                    className=" flex flex-col items-center p-4 tracking-wide"
-                    key={index}
-                  >
-                    <img
-                      src={each.profilePhoto[each.profilePhoto.length - 1]}
-                      alt={each.profilePhoto[each.profilePhoto.length - 1]}
-                      className="h-24 w-24 rounded-full object-cover"
-                    />
-                    <h1 className="mt-2 font-semibold">{each.fname}</h1>
-                    <p className="text-xs">
-                      {each.city}, {each.country}
-                    </p>
-                  </Link>
-                ))}
-              </div>
-            ))}
+          <div className="grid grid-cols-4 gap-4 ">
+            {uniData.map((uni) =>
+              uni.map((each, index) => (
+                <Link
+                  to={
+                    user._id == each._id
+                      ? `/user/profile/${each?._id}/`
+                      : `/user/univ/${each?._id}`
+                  }
+                  className=" flex flex-col items-center rounded-lg border-2  border-primary3 p-4 tracking-wide"
+                  key={index}
+                >
+                  <img
+                    src={each.profilePhoto[each.profilePhoto.length - 1]}
+                    alt={each.profilePhoto[each.profilePhoto.length - 1]}
+                    className="h-24 w-24 rounded-full object-cover"
+                  />
+                  <h1 className="mt-2 font-semibold">{each.fname}</h1>
+                  <p className="text-xs">
+                    {each.city}, {each.country}
+                  </p>
+                </Link>
+              )),
+            )}
           </div>
         </div>
       ) : (
